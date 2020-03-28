e4fcb093bcedcf70cf381ed5a57466f2a77e165d
DaveenDev <daveendev07@gmail.com>
2020-03-28 15:53:46 +0800

    Added votes answer styling views counter to question item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -10,6 +10,17 @@
                 <div class="card-body">
                     @foreach ($questions as $q)
                         <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{$q->votes}}</strong>{{Str::plural('vote',$q->vote)}}
+                                </div>
+                            <div class="status {{$q->status}}">
+                                    <strong>{{$q->answers}}</strong>{{Str::plural('answers',$q->vote)}}
+                                </div>
+                                <div class="view">
+                                    {{$q->views . " " . Str::plural('views',$q->vote)}}
+                                </div>
+                            </div>
                             <div class="media-body">
                             <h3 class="mt-0"> <a href="{{$q->url}}">{{$q->title}}</a></h3>
                             <p class="lead">
