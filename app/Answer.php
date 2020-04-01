e5e2391a9befbe528a20af94aed825602ea89858
DaveenDev <daveendev07@gmail.com>
2020-04-01 17:44:07 +0800

    Displayed answers for a selected question

--- app/Answer.php
+++ app/Answer.php
@@ -13,4 +13,15 @@ class Answer extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function getBodyHtmlAttribute(){
+        //parse the markdown into html ex. \n into <br>
+       return  \Parsedown::instance()->text($this->body);
+    }
+
+       public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
+
+
 }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-11">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
@@ -12,8 +12,7 @@
                         <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">back</a>
                         </div>
                     </div>
-                    
-
+                  
                 </div>
 
                 <div class="card-body">
@@ -26,6 +25,54 @@
                                         <button type="Submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                             </form>
                         @endif  
+                        <div class="float-right">
+                            <span class="text-muted">Created: {{$question->created_date}}</span>
+                            <div class="media">
+                                <a href="{{$question->user->url}}" class="pr-2">
+                                    <img src="{{$question->user->avatar}}"> 
+                                </a>
+                            
+                            <div class="media-body">
+                                <a href="{{$question->user->url}}">
+                                    {{$question->user->name}}
+                                </a>
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h3>{{$question->answers_count. " " . Str::plural('Answer',$question->answers_count)}}</h3>
+                    </div>
+                    <hr>
+                    @foreach($question->answers as $answer)
+                    <div class="media">
+                        <div class="media-body">
+                            {!! $answer->body_html !!}
+       
+                            <div class="float-right">
+                                <span class="text-muted">Answered: {{$answer->created_date}}</span>
+                                <div class="media">
+                                    <a href="{{$answer->user->url}}" class="pr-2">
+                                        <img src="{{$answer->user->avatar}}"> 
+                                    </a>
+                                
+                                <div class="media-body">
+                                    <a href="{{$answer->user->url}}">
+                                        {{$answer->user->name}}
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr>
+                    @endforeach
                 </div>
             </div>
         </div>
