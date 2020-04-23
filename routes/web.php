6034f61fc96de0b5b415a05a2aefb5bdc32999fa
DaveenDev <daveendev07@gmail.com>
2020-04-24 04:16:56 +0800

    setup spa to load only 1 page

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::view('/{any}','spa')->where('any','.*'); //return only spa view for all url request
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,15 +22,22 @@ Auth::routes();
 
 Route::get('/home', 'QuestionsController@index')->name('home');
 
-Route::resource('questions','QuestionsController')->except('show');
-//Route::post('/questions/{question}/answers','AnswerController@store')->name('answers.store');
-Route::resource('questions.answers','AnswerController')->except('create','show');
+//Auth::routes(['verify'=>true]);
+//Route::midleware('verified')->group(function(){
+
+    Route::resource('questions','QuestionsController')->except('show');
+    //Route::post('/questions/{question}/answers','AnswerController@store')->name('answers.store');
+    Route::resource('questions.answers','AnswerController')->except('create','show');
+    
+    Route::post('/answers/{answer}/accept','AcceptAnswerController')->name('answers.accept');
+    
+    Route::post('/questions/{question}/favorites','FavoritesController@store')->name('questions.favorite');
+    Route::delete('/questions/{question}/favorites','FavoritesController@destroy')->name('questions.unfavorite');
+    
+    
+    Route::post('/questions/{question}/vote','VotesController@vote_question');
+    Route::post('/answers/{answer}/vote','VotesController@vote_answer');
+    
+//});
+
 Route::get('/questions/{slug}','QuestionsController@show')->name('questions.show');
-Route::post('/answers/{answer}/accept','AcceptAnswerController')->name('answers.accept');
-
-Route::post('/questions/{question}/favorites','FavoritesController@store')->name('questions.favorite');
-Route::delete('/questions/{question}/favorites','FavoritesController@destroy')->name('questions.unfavorite');
-
-
-Route::post('/questions/{question}/vote','VotesController@vote_question');
-Route::post('/answers/{answer}/vote','VotesController@vote_answer');
